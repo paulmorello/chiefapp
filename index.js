0139e40dd60fb21dafa7f60448507cfad0e26ae8
paulmorello <paulmorello1@gmail.com>
2019-01-14 22:11:25 -0500

    added error messages

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ let isErrorHidden = true;
 let isSuccessHidden = true;
 
 getEmailAddress = () => {
-  let email = document.getElementById("email");
+  console.log('clicked');
+  let email = document.getElementById("email").value;
+  console.log(email);
   if (email) {
-    return email.value;
+    return email;
   } else {
     email = "This is not a valid email address";
     return email;
@@ -18,14 +20,18 @@ getEmailAddress = () => {
 }
 
 signUpBtn.addEventListener('click', function() {
+  console.log('clicked');
   let email = getEmailAddress();
   let firstName = document.getElementById('first-name').value;
   let lastName = document.getElementById('last-name').value;
   let message = document.getElementById('message')
 
+  console.log(email, firstName, lastName);
+
   let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (regex.test(email) && firstName && lastName) {
+    console.log('test passed');
     // identify requestor by email
     woopra.identify({
       email: email,
@@ -39,17 +45,28 @@ signUpBtn.addEventListener('click', function() {
       last_name: lastName
     });
 
+    console.log(message);
+
     // show success message
     message.style.background = 'lightgreen';
     message.style.color = 'green';
     message.style.width = '40%';
+    message.style.height = '60px';
+    message.style.margin = '20px auto';
     message.value = 'Success. You will receive a personal email';
 
+    console.log(message);
+
   } else {
+    console.log(message.value);
     // show fail message
     message.style.background = 'mistyrose';
     message.style.color = 'red';
     message.style.width = '40%';
+    message.style.height = '60px';
+    message.style.margin = '20px auto';
     message.value = 'Please make sure all fields are correct';
+
+    console.log(message.value);
   }
 });
